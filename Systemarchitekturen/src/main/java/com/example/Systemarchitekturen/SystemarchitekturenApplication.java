d344b9215525029116b58d95d7d7898841c0cc1c
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-04-05 21:29:58 +0200

    Add files via upload

---
+++ Systemarchitekturen/src/main/java/com/example/Systemarchitekturen/SystemarchitekturenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Systemarchitekturen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemarchitekturenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemarchitekturenApplication.class, args);
+	}
+
+}
