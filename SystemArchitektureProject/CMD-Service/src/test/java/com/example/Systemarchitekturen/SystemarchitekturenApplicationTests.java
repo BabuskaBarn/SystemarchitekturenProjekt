f9c1c135a55f30fce6088187010fa88c41e5a239
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-04-05 21:43:57 +0200

    Add files via upload

---
+++ SystemArchitektureProject/CMD-Service/src/test/java/com/example/Systemarchitekturen/SystemarchitekturenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Systemarchitekturen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemarchitekturenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
