9d25c29997cc11bc4da8db76064fa447f8369b5a
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-04-21 16:48:06 +0200

    Command side wurden die creates hinzugefügt und der Eventbus wurde erweitert aber der processing dh richtig abspeichern der events Fehlt noch

--- HotelManagementProject/eventbus/src/main/java/at/fhv/sys/eventbus/client/QueryClient.java
+++ HotelManagementProject/eventbus/src/main/java/at/fhv/sys/eventbus/client/QueryClient.java
@@ -6,7 +6,7 @@ import at.fhv.sys.hotel.commands.shared.events.RoomCreated;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
