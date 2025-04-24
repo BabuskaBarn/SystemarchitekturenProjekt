9b69518d472dc143554e3585d987777ef43c1f31
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-04-24 12:56:04 +0200

    Habe die Jackson configs vergessen zu pushen dammit booking funktioniert

---
+++ HotelManagementProject/eventbus/src/main/java/at/fhv/sys/eventbus/Config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package at.fhv.sys.eventbus.Config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.quarkus.jackson.ObjectMapperCustomizer;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class JacksonConfig implements ObjectMapperCustomizer {
+    @Override
+    public void customize(ObjectMapper objectMapper) {
+        // Java 8 Date/Time Unterstützung aktivieren
+        objectMapper.registerModule(new JavaTimeModule());
+        // Dates als Strings statt Timestamps schreiben
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+}
